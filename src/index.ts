84b63ab42771bc9a8ee35662423c0cf44948e3f4
Lord Kay <offeilord@gmail.com>
2022-04-16 13:38:31 +0000

    Kairos sms class definition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-class KairosSMS {
-    constructor() {
-    }
+import { IKairosSMSOptions } from './types/interfaces';
+import { Api } from './api';
+import { AxiosObservable } from 'axios-observable';
+import { APIEndpoints } from './constants/api-endpoints.constants';
 
-    static create(): KairosSMS {
-        return new KairosSMS()
-    }
+class KairosSMS {
+  private readonly options: IKairosSMSOptions;
+  constructor(options: IKairosSMSOptions) {
+    this.options = options;
+  }
+  static create(options: IKairosSMSOptions) {
+    return new KairosSMS(options);
+  }
+
+  send<T>(body: T): AxiosObservable<any> {
+    return Api(this.options).post(APIEndpoints.SEND_QUICK_SMS, {});
+  }
 }
 
-export default KairosSMS
-export {KairosSMS}
+export default KairosSMS;
+export { KairosSMS };
+
+const lord = KairosSMS.create({ apiKey: 'xxxxx', apiSecret: 'xxxxx' });
