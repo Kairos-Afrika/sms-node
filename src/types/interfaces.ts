cd3bee6408dc47831ee3bbb111ef6d781f12d1ac
Lord Kay <offeilord@gmail.com>
2022-04-16 13:38:50 +0000

    add interfaces

---
+++ src/types/interfaces.ts
@@ -0,0 +1,46 @@
+export interface IKairosSMSOptions {
+  /**
+   * API Key for the sms
+   * @description A generated API Key from the Kairos Afrika SMS Portal (https://kairosafrika.com)
+   */
+  apiKey: string;
+  /**
+   * API Secret for the sms
+   * @description A generated secret from the  Kairos Afrika SMS Portal (https://kairosafrika.com)
+   */
+  apiSecret: string;
+  /**
+   * API call timeout
+   * @description The amount of time in milliseconds which the api call should abort the request
+   * @default 8000
+   */
+  timeout?: number;
+}
+
+export interface ISMSBody {
+  /**
+   * MSISDN
+   * @description Contact number of which the message is to be sent to
+   */
+  to: string;
+
+  /**
+   * SENDER ID
+   * @description The originator of the message, these sender ids are created on the kairos afrika's portal and requires approval to be able to use it
+   */
+  from: string;
+
+  /**
+   * MESSAGE
+   * @description The content of the message to be sent to the 3rd party
+   */
+  message: string;
+  /**
+   * TYPE
+   * @description How the message should be shown on the 3rd party's handset, Accepts one of two values ['Quick', 'Flash']
+   * @default Quick
+   */
+  type?: ISMSType;
+}
+
+type ISMSType = 'Quick' | 'Flash';
