ba78666574dfa0b74fade6d5ea89c24015a801a5
Lord Kay <offeilord@gmail.com>
2022-04-16 13:37:57 +0000

    add base class for the api calls

---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import { IKairosSMSOptions } from './types/interfaces';
+import Axios from 'axios-observable';
+import { APIEndpoints } from './constants/api-endpoints.constants';
+
+export const Api = (credentials: IKairosSMSOptions) => {
+  return Axios.create({
+    baseURL: APIEndpoints.BASE_URL,
+    timeout: credentials?.timeout ?? 8000,
+    headers: {
+      'x-api-key': credentials?.apiKey,
+      'x-api-secret': credentials?.apiSecret,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+};
