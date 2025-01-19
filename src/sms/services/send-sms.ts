8ba83709c90fba47b9b12770b8c7e02c177a9304
Gilles Ashley <xylaray37@gmail.com>
2025-01-19 14:06:43 +0000

    KAAPI-68 Remove asQuickMultipleMSISDN method and its references from Kairos SMS tests and service implementation

--- src/sms/services/send-sms.ts
+++ src/sms/services/send-sms.ts
@@ -60,24 +60,6 @@ class SendSms {
       );
   }
 
-  asQuickMultipleMSISDN() {
-    return Api(this.config)
-      .post(APIEndpoints.SEND_QUICK_MULTIPLE_MSISDN, this.data as ISingleSMSBody)
-      .pipe(
-        map((response) => buildSMSResponse(HttpStatusCode.OK, `SMS successfully scheduled`, response?.data, true)),
-        catchError((err) =>
-          of(
-            buildSMSResponse(
-              err?.response?.status ?? HttpStatusCode.INTERNAL_SERVER_ERROR,
-              err?.response?.data?.message,
-              err,
-              false,
-            ),
-          ),
-        ),
-      );
-  }
-
   /**
    * Send sms as bulk sms
    */
